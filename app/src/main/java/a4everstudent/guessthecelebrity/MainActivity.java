0b238f202b35bb6e23c3f3d6a27bb54fc2bbc3ae
4EverStudent <mail@4everstudent.com>
2016-10-14 11:18:47 +0100

    Add code to show the celebrity image on ImageView.

--- app/src/main/java/a4everstudent/guessthecelebrity/MainActivity.java
+++ app/src/main/java/a4everstudent/guessthecelebrity/MainActivity.java
@@ -1,19 +1,61 @@
 package a4everstudent.guessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int chosenCeleb = 0;
+
+    ImageView imageView;
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String...urls){
+
+            try {
+
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
 
     public class DownloadTask extends AsyncTask<String, Void, String>{
         @Override
@@ -54,6 +96,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.imageView);
         DownloadTask task = new DownloadTask();
         String result;
 
@@ -63,13 +106,32 @@ public class MainActivity extends AppCompatActivity {
             //split the page so we don't get the images from the sideBar
             String[] splitResult = result.split("<div class=\"sidebarContainer\">");
 
-
-            Pattern p = Pattern.compile("alt=\"(.*?)\"");
+            Pattern p = Pattern.compile("src=\"(.*?)\"");
             Matcher m = p.matcher(splitResult[0]);
 
             while(m.find()){
-                System.out.println(m.group(1));
+                celebURLs.add(m.group(1));
             }
+
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while(m.find()){
+               celebNames.add(m.group(1));
+            }
+
+            Random random = new Random();
+            chosenCeleb = random.nextInt(celebURLs.size());
+
+            ImageDownloader imageTask = new ImageDownloader();
+
+            Bitmap celebImage;
+
+            celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+
+            imageView.setImageBitmap(celebImage);
+
+
 
 
         } catch (InterruptedException e) {
